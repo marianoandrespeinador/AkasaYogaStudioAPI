e5781912c82e6ee06f4670caef5baa99d33125a5
Nano <nanopp@gmail.com>
2017-07-05 20:31:30 -0600

    Added lesson cost

---
+++ AkasaYogaStudioAPI/Controllers/LessonCostController.cs
@@ -0,0 +1,16 @@
+﻿using Akasa.Dto;
+using Akasa.Services.Contracts;
+using AkasaYogaStudioAPI.Core;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AkasaYogaStudioAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class LessonCostController : AkasaController<ILessonCostService, LessonCostGetDto, LessonCostInsertDto, LessonCostUpdateDto>
+    {
+        public LessonCostController(ILessonCostService LessonCostService) 
+            : base(LessonCostService)
+        {
+        }
+    }
+}
